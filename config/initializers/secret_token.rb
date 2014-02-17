fffe653effa6b0fe6ecb7df6f504076806470bfe
Estelleabn <estelleaubouin@gmail.com>
2014-02-17 09:49:40 +0100

    First commit, guys

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urssflat::Application.config.secret_key_base = '1b109aeb7e3c618209f1f54dd4dae4b2e4b553409a333c4880c4cec3b01b80bfca9f9f3bf886dc7eabfb7745d9db5065fc21d6b39a1391c6b41389fc0bf502a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Urssflat::Application.config.session_store :cookie_store, key: '_urssflat_session'
